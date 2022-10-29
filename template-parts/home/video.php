9c487f884c330f8ba5bebfda82352006cae84855
Noman Nawaz <nomannomi306@gmail.com>
2022-10-29 18:47:44 +0500

    version 1.0.9

--- template-parts/home/video.php
+++ template-parts/home/video.php
@@ -1,1 +1,9 @@
-<?php echo do_shortcode( '[elementor-template id="1975"]' ); ?>
+<div id="videoAutoplay">
+    <?php echo do_shortcode( '[elementor-template id="124"]' ); ?>
+    <div class="video-reloader" data-video-target="videoAutoplay" style="display: none;">
+        <span><i class="fas fa-rotate-right"></i></span>
+    </div>
+    <div class="scroll-to" data-target="header">
+        <span><i class="fas fa-chevron-down"></i></span>
+    </div>
+</div>
