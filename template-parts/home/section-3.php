80dc2c384869105b38f4b8df2bee47b5c583e7bf
Noman Nawaz <nomannomi306@gmail.com>
2022-11-02 02:20:03 +0500

    version 1.2.0

--- template-parts/home/section-3.php
+++ template-parts/home/section-3.php
@@ -1,4 +1,4 @@
-<div id="section3" class="section-container section-3 section-sticky position-relative text-light w-100 px-site py-5 d-flex row mx-0">
+<div id="section3" class="section-container section-3 section-sticky position-relative text-light w-100 min-vh-100 px-site py-5 d-flex row mx-0">
     <div class="col-12 h-100 position-absolute top-0 start-0 px-0">
         <?php echo do_shortcode( '[nk_awb awb_type="image" awb_image="' . get_stylesheet_directory_uri() . '/assets/img/section-3.png' . '" awb_parallax="scroll" awb_parallax_mobile="true" awb_styles="width:100%; height:100%;"]' ); ?>
     </div>
@@ -7,7 +7,7 @@
             <h2 class="section-heading me-md-5"><?php _e( 'The Selection Process', 'btr' ); ?></h2>
         </div>
     </div>
-    <div class="col-12 col-md-6 order-2 order-md-2 d-flex align-items-md-end px-0 position-relative">
+    <div class="col-12 col-md-6 order-2 order-md-2 d-flex px-0 position-relative">
         <div class="section-content">
             <ul class="content-list list-group ps-0">
                 <li class="content list-group-item bg-transparent text-light px-0 py-3 rounded-0 border-0 border-bottom border-light">
