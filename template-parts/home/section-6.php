bc0c5fc8394566e090d5b1b08b02ae41fde7aa1f
Noman Nawaz <nomannomi306@gmail.com>
2022-11-02 09:15:52 +0500

    version 1.2.0

--- template-parts/home/section-6.php
+++ template-parts/home/section-6.php
@@ -2,12 +2,12 @@
     <div class="col-12 h-100 position-absolute top-0 start-0 px-0">
         <?php echo do_shortcode( '[nk_awb awb_type="image" awb_image="' . get_stylesheet_directory_uri() . '/assets/img/section-6.png' . '" awb_parallax="scroll" awb_parallax_mobile="true" awb_styles="width:100%; height:100%;"]' ); ?>
     </div>
-    <div class="col-12 col-md-6 order-1 order-md-1 mt-3 mt-md-5 mt-lg-10 px-0 position-relative">
+    <div class="col-12 col-md-6 mt-3 mt-md-5 mt-lg-10 px-0 position-relative">
         <div class="section-intro sticky-top" sticky-top="30">
             <h2 class="section-heading me-md-5"><?php _e( 'Our Commitment', 'btr' ); ?></h2>
         </div>
     </div>
-    <div class="col-12 col-md-6 order-2 order-md-2 d-flex px-0 position-relative">
+    <div class="col-12 col-md-6 d-flex px-0 position-relative">
         <div class="section-content">
             <ul class="content-list list-group ps-0">
                 <li class="content list-group-item bg-transparent text-light px-0 py-3 rounded-0 border-0 border-bottom border-light">
