015e7f48a53a38f745072a4ee95f8d733b870a4a
Noman Nawaz <nomannomi306@gmail.com>
2022-11-02 09:43:58 +0500

    version 1.2.1

--- template-parts/home/section-5.php
+++ template-parts/home/section-5.php
@@ -1,8 +1,8 @@
-<div id="section5" class="section-container section-5 section-sticky position-relative text-light w-100 min-vh-100 px-site py-5 d-flex row mx-0">
+<div id="section5" class="section-container section-5 section-sticky position-relative text-light w-100 min-vh-100 px-site py-5 d-flex flex-column flex-md-row row mx-0">
     <div class="col-12 h-100 position-absolute top-0 start-0 px-0">
         <?php echo do_shortcode( '[nk_awb awb_type="image" awb_image="' . get_stylesheet_directory_uri() . '/assets/img/section-5.png' . '" awb_parallax="scroll" awb_parallax_mobile="true" awb_styles="width:100%; height:100%;"]' ); ?>
     </div>
-    <div class="col-12 col-md-6 mt-3 mt-md-5 mt-lg-10 px-0 position-relative">
+    <div class="col-12 col-md-6 mt-5 px-0 position-relative">
         <div class="section-intro sticky-top" sticky-top="30">
             <h2 class="section-heading me-md-5"><?php _e( 'Lifetime Membership', 'btr' ); ?></h2>
         </div>
